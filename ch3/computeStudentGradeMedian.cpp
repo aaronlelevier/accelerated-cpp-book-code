3a59f0826b1a79f1e89d3d75079d7d0be767cf04
aaronlelevier <aaron.lelevier@gmail.com>
2019-03-06 05:12:43 -0800

    computeStudentGradeMedian.cpp - calculating final grade w/ median - example from book

--- ch3/computeStudentGradeMedian.cpp
+++ ch3/computeStudentGradeMedian.cpp
@@ -49,17 +49,25 @@ int main()
   typedef vector<double>::size_type vec_sz;
   vec_sz size = homework.size();
 
-  if (size == 0) {
-    cout << endl << "you must enter your grades. Please try again." << endl;
+  if (size == 0)
+  {
+    cout << endl
+         << "you must enter your grades. Please try again." << endl;
     return 1;
   }
 
   // sort in place
   sort(homework.begin(), homework.end());
 
+  // compute the median
+  vec_sz mid = size / 2;
+  double median = size % 2 == 0 ? (homework[mid] + homework[mid - 1] / 2)
+                                : homework[mid];
+
   // write the result
   streamsize prec = cout.precision();
   cout << "Your final grade is " << setprecision(3)
-       << 0.2 * midterm + 0.4 * final + 0.4 * sum / count << setprecision(prec) << endl;
+       << 0.2 * midterm + 0.4 * final + 0.4 * median
+       << setprecision(prec) << endl;
   return 0;
 }
