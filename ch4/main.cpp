d31adb6c9239b10f1924bbf25711593100a5885d
aaronlelevier <aaron.lelevier@gmail.com>
2019-03-15 05:27:11 -0700

    student info

--- ch4/main.cpp
+++ ch4/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include "assert.h"
+
 #include "median.h"
+#include "Student_info.h"
 
 int main()
 {
@@ -11,6 +14,14 @@ int main()
   homework.push_back(20);
 
   std::cout << "median: " << median(homework) << std::endl;
+
+  // student info
+  Student_info student = {"Bob", 15};
+  student.homework = homework;
+
+  Student_info student2 = {"Aaron", 5};
+
+  assert(compare(student2, student));
 
   return 0;
 }
